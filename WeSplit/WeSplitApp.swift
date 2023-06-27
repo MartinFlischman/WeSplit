c3abeae03210324ac9eb6c2c2849a44b163ad7df
Martin Flischman <wipes_meshes.03@icloud.com>
2023-06-27 12:43:08 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Martin Flischman on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
