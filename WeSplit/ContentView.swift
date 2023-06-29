af988510510d33f60c9df9423c995db077e4e2c2
Martin Flischman <wipes_meshes.03@icloud.com>
2023-06-29 16:04:00 +0200

    Feature update
    feat: Change made so that the user can choose any tip percentage to give between 0 -100.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -61,7 +61,7 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
